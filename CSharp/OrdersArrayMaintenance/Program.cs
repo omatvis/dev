e71c7f50d0f3fed0df79071088ce06746eb31480
olemat <omatvis@softserveinc.com>
2024-01-26 16:46:56 +0200

    parse a string of orders, sort the orders and tag possible errors

---
+++ CSharp/OrdersArrayMaintenance/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OrdersArrayMaintenance;
+
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+        string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+        string[] orders = orderStream.Split(",");
+        Array.Sort(orders);
+        foreach (string order in orders)
+        {
+            Console.Write(order);
+            if (order.Length != 4)
+            {
+                Console.WriteLine("     - Error");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
