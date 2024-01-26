0bc56c78ca1330f26349285c449d52970fa98fe6
olemat <omatvis@softserveinc.com>
2024-01-26 16:37:47 +0200

    Reverse characters for each word in sentence

---
+++ CSharp/ReverseWorkdsInSentence/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReverseWorkdsInSentence;
+
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+        string pangram = "The quick brown fox jumps over the lazy dog";        
+        string[] words = pangram.Split(' ');
+        foreach (string word in words)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Reverse(chars);
+            Console.Write(new string(chars) + " ");
+        }
+    }
+}
