4cdc6f6f1d39c242c97fcdb1759097490ea3ed11
olemat <omatvis@softserveinc.com>
2024-02-14 19:30:41 +0200

    Pass argument by value or reference.

---
+++ CSharp/CircleMaths/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CircleMaths;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] students = ["Jenna", "Ayesha", "Carlos", "Viktor"];
+
+        DisplayStudents(students);
+        DisplayStudents(["Robert", "Vanya"]);
+
+        static void DisplayStudents(string[] students)
+        {
+            foreach (string student in students)
+            {
+                Console.Write($"{student}, ");
+            }
+            Console.WriteLine();
+        }
+
+        PrintCircleArea(12);
+
+        static void PrintCircleArea(int radius)
+        {
+            double pi = 3.14159;
+            double area = pi * (radius * radius);
+            Console.WriteLine($"Area = {area}");
+        }
+    }
+}
---
+++ CSharp/ReferenceAndValueType/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ReferenceAndValueType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 4;
+        int c = 0;
+
+        Multiply(a, b, c);
+        Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+        static void Multiply(int a, int b, int c)
+        {
+            c = a * b;
+            Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+        }
+
+        int[] array = [1, 2, 3, 4, 5];
+
+        PrintArray(array);
+        Clear(array);
+        PrintArray(array);
+
+        void PrintArray(int[] array)
+        {
+            foreach (int a in array)
+            {
+                Console.Write($"{a} ");
+            }
+            Console.WriteLine();
+        }
+
+        void Clear(int[] array)
+        {
+            Array.Clear(array);
+            Array.Fill<int>(array, 0);
+        }
+
+        string status = "Healthy";
+
+        Console.WriteLine($"Start: {status}");
+        SetHealth(false);
+        Console.WriteLine($"End: {status}");
+
+        void SetHealth(bool isHealthy)
+        {
+            status = (isHealthy ? "Healthy" : "Unhealthy");
+            Console.WriteLine($"Middle: {status}");
+        }
+    }
+}
