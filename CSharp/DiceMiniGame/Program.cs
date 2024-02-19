be676be4c6258b236c6686f6f92ca4c7ceb4f9dc
olemat <omatvis@softserveinc.com>
2024-02-19 18:04:07 +0200

    Dice mini game

---
+++ CSharp/DiceMiniGame/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Formats.Asn1;
+
+namespace DiceMiniGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Random random = new Random();
+
+        Console.WriteLine("Would you like to play? (Y/N)");
+        if (ShouldPlay())
+        {
+            PlayGame();
+        }
+
+        void PlayGame()
+        {
+            var play = true;
+
+            while (play)
+            {
+                var target = RollTarget();
+                var roll = RollUser();
+
+                Console.WriteLine($"Roll a number greater than {target} to win!");
+                Console.WriteLine($"You rolled a {roll}");
+                Console.WriteLine(WinOrLose(roll, target));
+                Console.WriteLine("\nPlay again? (Y/N)");
+
+                play = ShouldPlay();
+            }
+        }
+
+        bool ShouldPlay()
+        {
+            string? answer = Console.ReadLine();
+            return (answer is not null) ? answer.ToLower().Equals("y"): false;
+        }
+
+        string WinOrLose(int roll, int target )
+        {
+            return (roll > target) ? "You win!": "You lose!";
+        }
+
+        int RollTarget()
+        {
+            return new Random().Next(1, 6);
+        }
+
+        int RollUser()
+        {
+            return new Random().Next(1, 7);
+        }
+    }
+}
