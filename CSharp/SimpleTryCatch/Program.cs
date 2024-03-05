21d4ec7f05a78720d46f2ab64d1196589025712b
olemat <omatvis@softserveinc.com>
2024-03-05 10:44:04 +0200

    simple try catch statement

---
+++ CSharp/SimpleTryCatch/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleTryCatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Process1();
+        }
+        catch
+        {
+            Console.WriteLine("An exception has been caught");
+        }
+        Console.WriteLine("Exit program");
+    }
+
+    private static void Process1()
+    {
+        WriteMessage();
+    }
+
+    private static void WriteMessage()
+    {
+        double float1 = 3000.0;
+        double float2 = 0.0;
+        int number1 = 3000;
+        int number2 = 0;
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+}
