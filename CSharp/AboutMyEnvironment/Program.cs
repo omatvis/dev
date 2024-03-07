9f36cc391e4c60828b4901f231dd11952da90712
olemat <omatvis@softserveinc.com>
2024-03-07 17:32:20 +0200

    Environment constants

---
+++ CSharp/AboutMyEnvironment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AboutMyEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine(Environment.OSVersion.VersionString);
+    }
+}
