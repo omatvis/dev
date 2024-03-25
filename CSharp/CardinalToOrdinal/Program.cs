23f9948e240457a4525784647b0324f991bbcffd
olemat <omatvis@softserveinc.com>
2024-03-25 13:11:13 +0200

    Cardinal to ordinal conversion. Switch pattern matching

---
+++ CSharp/CardinalToOrdinal/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CardinalToOrdinal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunCardinalToOrdinal();
+    }
+
+    static string CardinalToOrdinal(int number)
+    {
+        int lastTwoDigits = number % 100;
+        string ordinalText = lastTwoDigits switch
+        {
+             >= 11 and <=13 => $"{number:N0}th",
+            _
+                => (number % 10) switch
+                {
+                    1 => $"{number:N0}st",
+                    2 => $"{number:N0}nd",
+                    3 => $"{number:N0}rd",
+                    _ => $"{number:N0}th"
+                }
+        };
+
+        return ordinalText;
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (int i = 0; i < 150; i++)
+        {
+            Console.WriteLine($"{CardinalToOrdinal(i)}");
+        }
+    }
+}
