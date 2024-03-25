f311ec5c51c09a08f614cdc8873b8e0ea45d18f2
olemat <omatvis@softserveinc.com>
2024-03-25 14:58:34 +0200

    native .NET debug and trace technique using TextWriterTraceListener

---
+++ CSharp/Instrumenting/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Instrumenting;
+
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+        Console.WriteLine($"Writing to: {logPath}");
+        TextWriterTraceListener logFile = new(File.CreateText(logPath));
+        Trace.Listeners.Add(logFile);
+        Trace.AutoFlush = true;
+        Debug.WriteLine("Debug Message");
+        Trace.WriteLine("Trace Message");
+    }
+}
