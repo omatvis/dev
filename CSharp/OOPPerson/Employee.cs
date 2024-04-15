5e1d56e32943bf44e88aef96dc746547e5781a5d
olemat <omatvis@softserveinc.com>
2024-04-15 18:39:53 +0300

    OOP Basics

---
+++ CSharp/OOPPerson/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace OOPPerson;
+
+public class Employee : Person
+{
+    public string? EmployeeCode { get; set; }
+    public DateTime HireDate { get; set; }
+
+    public new void WriteToConsole()
+    {
+        Console.WriteLine(
+            "{0} was born on {1:dd/MM/yy} and hired on {2:dd/MM/yy}",
+            Name,
+            DateOfBirth,
+            HireDate
+        );
+    }
+
+    public override string ToString() {
+        return $"{Name} is a {base.ToString()}";
+    }
+}
