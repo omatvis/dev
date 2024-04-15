9827c66adef27958a0df62aaed352ad4f0c8fdeb
olemat <omatvis@softserveinc.com>
2024-04-15 18:53:03 +0300

    OOP

---
+++ CSharp/OOPPerson/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPPerson;
+
+public class Person : Object
+{
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public void WriteToConsole()
+    {
+        Console.WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+    }
+}
---
+++ CSharp/OOPPerson/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOPPerson;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee john = new()
+        {
+            Name = "John Jones",
+            DateOfBirth = new DateTime(1990, 7, 28)
+        };
+        john.WriteToConsole();
+
+        john.EmployeeCode = "JJ001";
+        john.HireDate = new DateTime(2014, 11, 23);
+        Console.WriteLine($"{john.Name} was hired on {john.HireDate:dd/MM/yy}");
+
+        Console.WriteLine(john.ToString());
+    }
+
+}
