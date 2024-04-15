5031e265c1ddabaf04342f2240c5af0566368042
olemat <omatvis@softserveinc.com>
2024-04-15 18:14:09 +0300

    Nullable Value and Reference Type

---
+++ CSharp/NullHandling/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace NullHandling;
+
+public class Address
+{
+    public string? Building;
+    public string Street = String.Empty;
+    public string City = String.Empty;
+    public string Region = String.Empty;
+}
---
+++ CSharp/NullHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NullHandling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int thisCannotBeNull = 4;
+        //thisCannotBeNull = null;
+        Console.WriteLine(thisCannotBeNull);
+        int? thisCouldBeBull = null;
+        Console.WriteLine(thisCouldBeBull);
+        Console.WriteLine(thisCouldBeBull.GetValueOrDefault());
+        thisCouldBeBull = 7;
+        Console.WriteLine(thisCouldBeBull);
+        Console.WriteLine(thisCouldBeBull.GetValueOrDefault());
+
+        Nullable<int> thisCouldAlsoBeNull = null;
+        thisCannotBeNull = 11;
+        Console.WriteLine(thisCouldAlsoBeNull);
+
+        Address address = new()
+        {
+            Building = null,
+            Street = null!,
+            City = "Lviv",
+            Region = "UK"
+        };
+        Console.WriteLine(address.Building?.Length);
+        Console.WriteLine(address.Street?.Length);
+
+    }
+}
