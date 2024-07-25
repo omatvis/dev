b7d91cdaa02328672686020114df01e9e9d969ee
olemat <omatvis@softserveinc.com>
2024-07-25 16:31:18 +0300

    new versus override

---
+++ CSharpNutshell/NewOverride/Program.cs
@@ -0,0 +1,33 @@
+﻿Overrider over = new Overrider();
+BaseClass b1 = over;
+over.Foo(); // Overrider.Foo
+b1.Foo(); // Overrider.Foo
+Hider h = new Hider();
+BaseClass b2 = h;
+h.Foo(); // Hider.Foo
+b2.Foo(); // BaseClass.Foo
+
+public class BaseClass
+{
+    public virtual void Foo()
+    {
+        Console.WriteLine("BaseClass.Foo");
+    }
+}
+
+public class Overrider : BaseClass
+{
+    public override void Foo()
+    {
+        base.Foo();
+        Console.WriteLine("Overrider.Foo");
+    }
+}
+
+public class Hider : BaseClass
+{
+    public new void Foo()
+    {
+        Console.WriteLine("Hider.Foo");
+    }
+}
