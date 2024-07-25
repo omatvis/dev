546948870f1c11fcb7dcc40a6636644a4f4788fb
olemat <omatvis@softserveinc.com>
2024-07-25 12:18:38 +0300

    Indexer

---
+++ CSharpNutshell/Indexers/Sentence.cs
@@ -0,0 +1,12 @@
+﻿namespace Indexers;
+
+public class Sentence
+{
+    string[] words = "The quick brow fox".Split();
+
+    public string this [int index] // indexer
+    {
+        get { return words[index]; }
+        set { words[index] = value; }
+    }
+}
