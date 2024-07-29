3ff11e2a05970b93b1ada6334b0f594465e08300
olemat <omatvis@softserveinc.com>
2024-07-29 19:39:49 +0300

    delegate multicast

---
+++ CSharpNutshell/ProgressReporter/Util.cs
@@ -0,0 +1,14 @@
+﻿namespace ProgressReporter;
+
+public delegate void ProgressReporter (int percentComplete);
+public class Util
+{
+    public static void HardWork(ProgressReporter p)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            p(i * 10); // Invoke delegate
+            System.Threading.Thread.Sleep(100); // Simulate hard work
+        }
+    }
+}
