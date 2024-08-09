fb39c3ef3058465cb795fb26ee9713778c108a27
olemat <omatvis@softserveinc.com>
2024-08-09 17:57:15 +0300

    Async Await

---
+++ Own/Tetris/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tetris;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine("Press any key combination");
+        ConsoleKeyInfo key = ReadKey();
+        WriteLine();
+        WriteLine($"Key: {key.Key}, Char: {key.KeyChar}, Modifiers: {key.Modifiers}");
+
+    }
+}
