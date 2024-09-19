ad4610acc5243bdf97cc59a54025ee983160fa8c
olemat <omatvis@softserveinc.com>
2024-09-19 19:26:09 +0300

    Classes Part 1

---
+++ Testing/Lib/PacktLib/BankAccount.cs
@@ -0,0 +1,8 @@
+namespace PacktLib.Shared;
+
+public class BankAccount
+{
+    public string? AccountName; // instance member
+    public decimal Balance; // instance member
+    public static decimal InterestRate; // shared member
+}
