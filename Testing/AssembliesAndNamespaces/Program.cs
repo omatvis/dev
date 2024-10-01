e5709547e7334c265753ab15099c8a868d1bfba6
olemat <omatvis@softserveinc.com>
2024-10-01 15:04:14 +0300

    Assemblies and NameSpaces

---
+++ Testing/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace AssembliesAndNamespaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XDocument doc = new();
+
+        string s1 = "Hello";
+        String s2 = "World";
+        WriteLine($"{s1} {s2}");
+    }
+}
