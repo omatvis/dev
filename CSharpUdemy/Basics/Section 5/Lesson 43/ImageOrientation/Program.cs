00c46a99ea883485f761d5284d9e3ba26131604d
olemat <omatvis@softserveinc.com>
2024-11-20 11:37:23 +0200

    Exercises for conditionals

---
+++ CSharpUdemy/Basics/Section 5/Lesson 43/ImageOrientation/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+** Write a program and ask the user to enter the width and height of an image.
+** Then tell if the image is landscape or portrait.
+**/
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the height of the image: ");
+        string? height = Console.ReadLine();
+        Console.Write("Enter the weight of the image: ");
+        string? weight = Console.ReadLine();
+        bool parseHeight = int.TryParse(height, out int heightValue);
+        bool parseWeight = int.TryParse(weight, out int weightValue);
+        if (parseHeight && parseWeight)
+        {
+            if (heightValue > weightValue)
+            {
+                Console.WriteLine("Portrait image orientation");
+            }
+            else
+            {
+                Console.WriteLine("Landscape image orientation");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Height or Weight values were entered in incorrect format!");
+        }
+    }
+}
---
+++ CSharpUdemy/Basics/Section 5/Lesson 43/MaxOfTwoNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+** Write a program which takes two numbers from the console and displays the maximum of the two.
+**/
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        string? firstNumber = Console.ReadLine();
+        Console.Write("Enter the second number: ");
+        string? secondNumber = Console.ReadLine();
+        bool isFirstNumber = int.TryParse(firstNumber, out int first);
+        bool isSecondNumber = int.TryParse(secondNumber, out int second);
+
+        if (isFirstNumber && isSecondNumber)
+        {
+            Console.WriteLine($"Maximum: {Math.Max(first, second)}");
+        }
+        else
+            Console.WriteLine("First or second number were entered in wrong format!");
+
+    }
+}
