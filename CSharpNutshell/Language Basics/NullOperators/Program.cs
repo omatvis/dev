7eb293d1faafc8461d91b8b0b52a12fae834961c
olemat <omatvis@softserveinc.com>
2024-12-27 12:54:45 +0200

    Nullable Operators

--- CSharpNutshell/Language Basics/NullOperators/Program.cs
+++ CSharpNutshell/Language Basics/NullOperators/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System.Data.SqlTypes;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string? s1 = null;
+        string s2 = s1 ?? "nothing";
+        string? s3 = null;
+        s3 ??= "nothing 2";
+
+        System.Text.StringBuilder? sb = null;
+        string? s = sb?.ToString().ToUpper();
+        Console.WriteLine($"{s1} {s2} {s3} {s}");
     }
 }
