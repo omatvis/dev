7519a178a294443b36809b0ab2b42a060cd5880f
olemat <omatvis@softserveinc.com>
2025-04-03 16:23:16 +0300

    Practicing c#12 classes etc,

---
+++ CSharp12Modern/Chapter05/PacktLibraryNetStandard2/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Shared;
+
+public class BankAccount
+{
+    public string? AccountName; // Instance member. It could be null.
+    public decimal Balance; // Instance member. Defaults to zero.
+    public static decimal InterestRate; // Shared member. Defaults to zero.
+}
---
+++ CSharp12Modern/Chapter05/PacktLibraryNetStandard2/WondersOfTheAncientWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Shared;
+
+[Flags]
+public enum WondersOfTheAncientWorld: byte
+{
+    GreatPyramidOfGiza = 0b000000001,
+    HangingGardensOfBabylon = 0b00000010,
+    StatueOfZeusAtOlympia = 0b00000100,
+    TempleOfArtemisAtEphesus = 0b00001000,
+    MausoleumAtHalicarnassus = 0b00010000,
+    ColossusOfRhodes = 0b00100000,
+    LighthouseOfAlexandria = 0b01000000
+}
