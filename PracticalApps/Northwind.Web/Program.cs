e04aef7543961e2d0e6ac14aa30da29508585c2a
olemat <omatvis@softserveinc.com>
2025-05-16 14:59:58 +0300

    Razor Pages

---
+++ PracticalApps/Northwind.Web/Program.cs
@@ -0,0 +1,57 @@
+using Northwind.EntityModels;
+
+namespace Northwind.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Configure the web server host and services.
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
+            builder.Services.AddNorthwindContext();
+            var app = builder.Build();
+            #endregion
+
+            #region Configure the HTTP pipeline and routes
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseHsts();
+            }
+
+            // Implementing an anonymous inline delegate as middleware
+            // to intercept HTTP requests and responses.
+            app.Use(async (HttpContext context, Func<Task> next) =>
+            {
+                RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+                if (rep is not null)
+                {
+                    WriteLine($"Endpoint name: {rep.DisplayName}");
+                    WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+                }
+                if (context.Request.Path == "/bonjour")
+                {
+                    // In the case of a match on URL path, this becomes a terminating
+                    // delegate that returns so does not call the next delegate.
+                    await context.Response.WriteAsync("Bonjour Monde!");
+                    return;
+                }
+                // We could modify the request before calling the next delegate.
+                await next();
+                // We could modify the response after calling the next delegate.
+            });
+
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles(); // index.html, default.html, and so on.
+            app.UseStaticFiles();
+
+    
+            app.MapGet("/hello", () => $"Environment is {app.Environment.EnvironmentName}");
+            app.MapRazorPages();
+            #endregion
+
+            app.Run();
+            WriteLine("This executes after the web server has stopped!");
+        }
+    }
+}
