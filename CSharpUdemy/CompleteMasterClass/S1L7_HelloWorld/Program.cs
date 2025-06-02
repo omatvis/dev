fa9ad6ad9e88adbe180959352c48c5d44e669298
olemat <omatvis@softserveinc.com>
2025-06-02 18:01:17 +0300

    Hello World and Landing Rocket Simulation app were added

---
+++ CSharpUdemy/CompleteMasterClass/S1L7_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace S1L7_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Oleg!");
+            var AggregateException = -100;
+            Console.ReadKey();
+        }
+    }
+}
