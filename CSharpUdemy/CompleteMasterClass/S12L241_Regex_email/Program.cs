1fd752296cccd955455257531d2c5af1fe7e2305
olemat <omatvis@softserveinc.com>
2025-09-09 14:20:28 +0300

    Regular Expression: extract email from a sentence

---
+++ CSharpUdemy/CompleteMasterClass/S12L241_Regex_email/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace S12L241_Regex_email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exercise = new Exercise();
+            exercise.ExtractPatterns("Contact us at support@example.com or sales@example.org.");
+            Console.ReadKey();
+        }
+    }
+
+    public class Exercise
+    {
+        public void ExtractPatterns(string input)
+        {
+            Regex regex = new Regex(@"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+
+}
