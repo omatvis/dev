29919c1ad8f0d2ffe4a0c399c5a68652e0d9b351
olemat <omatvis@softserveinc.com>
2025-09-12 17:20:00 +0300

    WPF Framework Learning: meet yaml :)

---
+++ CSharpUdemy/CompleteMasterClass/S13L251_Recreate_Thsi_GUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace S13L251_Recreate_Thsi_GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CSharpUdemy/CompleteMasterClass/S13L251_Recreate_Thsi_GUI/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S13L251_Recreate_Thsi_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // < Button Grid.Column = "4" Content = "B" Grid.Row = "3" />
+            Button button = new Button();
+            button.Content = "B";
+            Grid.SetColumn(button, 4);
+            Grid.SetRow(button, 3);
+            Grid? mainGrid = FindName("myGrid") as Grid;
+            mainGrid?.Children.Add(button);
+        }
+    }
+}
