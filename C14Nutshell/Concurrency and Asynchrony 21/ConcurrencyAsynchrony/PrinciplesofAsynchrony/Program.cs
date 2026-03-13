ee4350dfaf6e63f70160b034a1ed1ec2d8aae4b8
omatvis <omatvis@matvisiv.info>
2026-03-13 16:24:52 +0200

    Add async, task, and continuation demo projects
    Added PrinciplesofAsynchrony, TaskContinuation, and Tasks projects to the solution, each targeting .NET 10.0. Introduced example code for async prime counting, task continuations, and various task behaviors including exception handling and long-running tasks. These additions provide demonstrations of asynchrony and task management in C#.

---
+++ C14Nutshell/Concurrency and Asynchrony 21/ConcurrencyAsynchrony/PrinciplesofAsynchrony/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrinciplesofAsynchrony
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Task<int> GetPrimesCountAsync(int start, int count)
+            {
+                return Task.Run( () =>
+                  ParallelEnumerable.Range(start, count).Count(n =>
+                    Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+            }
+
+            async Task DisplayPrimeCounts()
+            {
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine(await GetPrimesCountAsync(i * 1000000 + 2, 1000000) +
+                      " primes between " + (i * 1000000) + " and " + ((i + 1) * 1000000 - 1));
+                Console.WriteLine("Done!");
+            }
+
+            await DisplayPrimeCounts();
+        }
+    }
+}
---
+++ C14Nutshell/Concurrency and Asynchrony 21/ConcurrencyAsynchrony/TaskContinuation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskContinuation
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The new thread is runnung ib background mode 
+            // So, if main thread ends up the thread is closed without waiting for the end of the execution
+            // So, we should block main thread to wait for result
+            Task<int> primeNumberTask = Task.Run(() =>
+              Enumerable.Range(2, 3000000).Count(n =>
+                Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+            var awaiter = primeNumberTask.GetAwaiter();
+            awaiter.OnCompleted(() =>
+            {
+                int result = awaiter.GetResult();
+                Console.WriteLine(result);
+                Console.WriteLine("The thread is completed!");
+            });
+            await primeNumberTask;
+        }
+    }
+}
