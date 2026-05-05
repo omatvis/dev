a9b8bae8ec88734a4aca83c4f5228dea0b0cab37
omatvis <omatvis@matvisiv.info>
2026-05-05 18:44:08 +0300

    Add Northwind EF Core context, models, and DI support
    Added two new projects for Northwind EF Core integration: DataContext and EntityModels, both targeting .NET 10.0. Introduced entity model classes for all Northwind tables and views with full data annotations and navigation properties. Implemented NorthwindContext with DbSet properties and model configuration. Added DI extension for context registration and a logger for EF Core SQL commands. Included a solution file referencing both projects.

---
+++ C14Modern/PracticalApps/Northwind.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Northwind.EntityModels
+{
+    public static class NorthwindContextExtensions
+    {
+        public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string connectionString)
+        {
+            return services.AddDbContext<NorthwindContext>(options =>
+                { 
+                    options.UseSqlServer(connectionString);
+                    options.LogTo(NorthwindContextLogger.WriteLine, [Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting]);
+                },ServiceLifetime.Transient, ServiceLifetime.Transient);
+        }
+    }
+}
---
+++ C14Modern/PracticalApps/Northwind.DataContext.SqlServer/NorthwindContextLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Environment;
+
+namespace Northwind.EntityModels
+{
+    public class NorthwindContextLogger
+    {
+        public static void WriteLine(string message)
+        {
+            string path = Path.Combine(GetFolderPath(SpecialFolder.DesktopDirectory), "NorthwindLog.txt");
+            StreamWriter textFile = File.AppendText(path);
+            textFile.WriteLine(message);
+            textFile.Close();
+        }
+    }
+}
