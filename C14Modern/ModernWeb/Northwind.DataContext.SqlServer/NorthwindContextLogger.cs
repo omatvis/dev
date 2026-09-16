96e431f789de471fd8bb14b56c1f3865338bf9c1
omatvis <omatvis@matvisiv.info>
2026-09-16 22:40:11 +0300

    Add .NET 10 EF Core solution for Northwind schema
    Introduced a new .NET 10 solution with EF Core and SQL Server, using central NuGet package management. Added projects for DbContext/logging and entity models. Configured connection string management and SQL logging. Defined all Northwind entities and views as C# classes. Expanded database with bulk sample data, new tables for demographics and regions, enforced relationships, and added stored procedures for advanced reporting.

---
+++ C14Modern/ModernWeb/Northwind.DataContext.SqlServer/NorthwindContextLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.DataContext.SqlServer
+{
+    public class NorthwindContextLogger
+    {
+        public static void WriteLog(string message)
+        {
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "book-logs");
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string dateTimeStamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string path = Path.Combine(folder, $"northwindlog-{dateTimeStamp}.txt");
+
+            using StreamWriter logFile = File.AppendText(path);
+            logFile.WriteLine(message);
+        }
+    }
+}
